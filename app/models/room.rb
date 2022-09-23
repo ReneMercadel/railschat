eee49aa786d3dfad2d16cc991ffb7a7be9b7263a
Rene Mercadel <renemercadel@gmail.com>
2022-09-23 14:57:51 -0500

    Implementing Messages scaffold and turbo stream broadcasting

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
+  has_many :messages
+
   # This allows the list of available rooms to update live from a user's view without refreshing
   broadcasts_to ->(room){:rooms_list}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # resources :messages
   devise_for :users
-  resources :rooms
+  
+  # Nesting "resources :messages" into "resources ;rooms"
+  resources :rooms do
+    resources :messages
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
